fe9ac26dde9acf377839cdb9b4ef78f91dc4c9d5
Karan02 <krnptl1234@gmail.com>
2020-05-27 14:21:59 +0530

    first commit

---
+++ models/product.js
@@ -0,0 +1,36 @@
+const products = []
+const fs = require("fs")
+const path = require("path")
+const p = path.join(path.dirname(process.mainModule.filename),"data","products.json")
+
+const getProductsFromFile = (cb) => {
+    
+    fs.readFile(p,(err,content)=>{
+        if(err){
+         cb([])
+        }else{
+        cb(JSON.parse(content))
+    }})
+
+}
+module.exports = class Product {
+    constructor(title){
+        this.title = title
+    }
+
+    save(){
+        getProductsFromFile(products =>{
+       
+            products.push(this)
+            fs.writeFile(p,JSON.stringify(products),(err)=>{
+                console.log(err)
+            })
+        })
+       
+    }
+
+    // static keyword is to refer to intial class and not instantiated object
+    static fetchAll(cb){
+       getProductsFromFile(cb)
+    }
+}
