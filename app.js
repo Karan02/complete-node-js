c24fc0053fe60bdffbed7ef3d35c8cf06eaca5f5
Karan02 <krnptl1234@gmail.com>
2020-06-04 21:08:27 +0530

    cookie session done

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
 const path = require('path');
-
+const getDb = require("./util/database").getDb
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
+const mongoose = require("mongoose")
 const errorController = require('./controllers/error');
-const User = require('./models/user');
+const session = require("express-session")
+const MongoDBStore = require("connect-mongodb-session")(session)
 
+// const mongoConnect = require("./util/database").mongoConnect
+// const sequelize = require('./util/database');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/order-item');
+const User = require("./models/user")
 const app = express();
-
+const store = new MongoDBStore({
+  uri:"mongodb://127.0.0.1:27017/localhost",
+  collection:"sessions"
+})
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const authRoutes = require('./routes/auth');
-
+const authRoutes = require("./routes/auth")
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('5bab316ce0a7c75f783cb8a8')
+//resave not saving on every request, makes it faster
+app.use(session({secret:"my secret",resave:false,saveUninitialized:false,store:store}))
+
+// this middleware used because we can have access to all cool methods mongoose have on model
+app.use((req,res,next)=>{
+  if(req.session.user){
+  User.findById(req.session.user._id)
     .then(user => {
-      req.user = user;
-      next();
+      // req.session.isLoggedIn = true;
+      // req.user = user;
+        req.user = user
+      next()
+    })
+    .catch(err => console.log(err));}
+    else{
+      next()
+    }
+})
+// this code runs many times
+app.use((req, res, next) => { 
+  User.findById("5ed7cc1b7b4c2702d867b8f4")
+    .then(user => {
+      // console.log("user",user)
+      req.user = user
+      next()
     })
     .catch(err => console.log(err));
 });
@@ -32,27 +63,65 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+
 app.use(errorController.get404);
 
-mongoose
-  .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
-  )
-  .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
-    app.listen(3000);
+// Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+// //has many is for one to many relations
+// User.hasMany(Product);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
+// Cart.belongsToMany(Product, { through: CartItem });
+// //belongs to many is used for many to many relations
+// Product.belongsToMany(Cart, { through: CartItem });
+// Order.belongsTo(User);
+// User.hasMany(Order);
+// Order.belongsToMany(Product, { through: OrderItem });
+// // below code runs only once
+// sequelize
+//   // .sync({ force: true })
+//   .sync()
+//   .then(result => {
+//     return User.findByPk(1);
+//     // console.log(result);
+//   })
+//   .then(user => {
+//     if (!user) {
+//       return User.create({ name: 'Max', email: 'test@test.com' });
+//     }
+//     return user;
+//   })
+//   .then(user => {
+//     // console.log(user);
+//     return user.createCart();
+//   })
+//   .then(cart => {
+//     app.listen(3000);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+// mongoConnect(() => {
+   
+//   app.listen(3000);
+//   console.log("Listening on port 3000");
+// })
+mongoose.connect("mongodb://localhost:27017/localhost").then((result)=>{
+User.findOne().then(user=>{
+if(!user){
+  const user =  User.create({
+    name:"Max",
+    email:"ken@gmail.com",
+    cart:{
+      items:[]
+    }
   })
-  .catch(err => {
-    console.log(err);
-  });
+}
+})  
+
+app.listen(3000)
+  console.log("Listening on port 3000");
+
+}).catch((err)=>{
+  console.log(err)
+})
