aaadc331b59b6570a7f4d4924757d45d05d3033c
Karan02 <krnptl1234@gmail.com>
2020-06-05 21:07:02 +0530

    authentication

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ const mongoose = require("mongoose")
 const errorController = require('./controllers/error');
 const session = require("express-session")
 const MongoDBStore = require("connect-mongodb-session")(session)
-
+// cross site request forgery
+const csrf = require("csurf")
+const flash = require("connect-flash")
 // const mongoConnect = require("./util/database").mongoConnect
 // const sequelize = require('./util/database');
 // const Product = require('./models/product');
@@ -21,6 +23,7 @@ const store = new MongoDBStore({
   uri:"mongodb://127.0.0.1:27017/localhost",
   collection:"sessions"
 })
+const csurfProtection = csrf()
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -32,7 +35,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //resave not saving on every request, makes it faster
 app.use(session({secret:"my secret",resave:false,saveUninitialized:false,store:store}))
+app.use(csurfProtection)
 
+
+app.use(flash())
+
+app.use((req,res,next)=>{
+  //locals provided by express, local variable that are passed in views
+  res.locals.isAuthenticated = req.session.isLoggedIn
+  res.locals.csrfToken = req.csrfToken()
+  next()
+})
 // this middleware used because we can have access to all cool methods mongoose have on model
 app.use((req,res,next)=>{
   if(req.session.user){
@@ -40,24 +53,29 @@ app.use((req,res,next)=>{
     .then(user => {
       // req.session.isLoggedIn = true;
       // req.user = user;
+      console.log("ulaa",user)
         req.user = user
       next()
     })
     .catch(err => console.log(err));}
     else{
+
+      console.log("in else")
       next()
     }
 })
+
+
 // this code runs many times
-app.use((req, res, next) => { 
-  User.findById("5ed7cc1b7b4c2702d867b8f4")
-    .then(user => {
-      // console.log("user",user)
-      req.user = user
-      next()
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => { 
+//   User.findById("5ed7cc1b7b4c2702d867b8f4")
+//     .then(user => {
+//       // console.log("user",user)
+//       req.user = user
+//       next()
+//     })
+//     .catch(err => console.log(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -107,17 +125,17 @@ app.use(errorController.get404);
 //   console.log("Listening on port 3000");
 // })
 mongoose.connect("mongodb://localhost:27017/localhost").then((result)=>{
-User.findOne().then(user=>{
-if(!user){
-  const user =  User.create({
-    name:"Max",
-    email:"ken@gmail.com",
-    cart:{
-      items:[]
-    }
-  })
-}
-})  
+// User.findOne().then(user=>{
+// if(!user){
+//   const user =  User.create({
+//     name:"Max",
+//     email:"ken@gmail.com",
+//     cart:{
+//       items:[]
+//     }
+//   })
+// }
+// })  
 
 app.listen(3000)
   console.log("Listening on port 3000");
