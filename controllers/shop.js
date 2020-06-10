265e87b4bfc20f7e514208baedf05e905b8ef811
Karan02 <krnptl1234@gmail.com>
2020-06-10 13:12:46 +0530

    done pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -3,14 +3,27 @@ const Order = require('../models/order');
 const fs = require("fs")
 const path = require("path")
 const PDFdocument = require("pdfkit")
+const ITEMS_PER_PAGE = 1
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1
+  let totalItems
+  Product.find().countDocuments().then(number=>{
+    totalItems = number
+    return Product.find().skip((page-1)*ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+  })
     .then(products => {
-      console.log(products);
       res.render('shop/product-list', {
         prods: products,
-        pageTitle: 'All Products',
-        path: '/products'
+        pageTitle: 'Products',
+        path: '/products',
+        currentPage:page,
+        totalItems: totalItems,
+        hasNextPage: ITEMS_PER_PAGE*page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page+ 1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
+
       });
     })
     .catch(err => {
@@ -38,12 +51,25 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1
+  let totalItems
+  Product.find().countDocuments().then(number=>{
+    totalItems = number
+    return Product.find().skip((page-1)*ITEMS_PER_PAGE).limit(ITEMS_PER_PAGE)
+  })
     .then(products => {
       res.render('shop/index', {
         prods: products,
         pageTitle: 'Shop',
-        path: '/'
+        path: '/',
+        currentPage:page,
+        totalItems: totalItems,
+        hasNextPage: ITEMS_PER_PAGE*page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page+ 1,
+        previousPage:page-1,
+        lastPage:Math.ceil(totalItems/ITEMS_PER_PAGE)
+
       });
     })
     .catch(err => {
