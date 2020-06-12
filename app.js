8434302fb51fa8d8b6079f58c3f5406c59ad2713
Karan02 <krnptl1234@gmail.com>
2020-06-12 15:58:59 +0530

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const express = require("express")
 const app = express()
 const bodyParser = require("body-parser")
@@ -5,6 +6,9 @@ const mongoose = require("mongoose")
 const feedRoutes = require("./routes/feed")
 // app.use(bodyParser.urlencoded()) //x-www-forn-urlencoded <form>
 app.use(bodyParser.json()) //application json
+
+//serving static files
+app.use("/images",express.static(path.join(__dirname,"images")))
 app.use((req,res,next)=>{
     res.setHeader("Access-Control-Allow-Origin","*")
     res.setHeader("Access-Control-Allow-Methods","OPTIONS,GET,POST,PUT,PATCH,DELETE")
@@ -12,6 +16,20 @@ app.use((req,res,next)=>{
     next()
 })
 app.use("/feed",feedRoutes)
-mongoose.connect("mongodb://")
-app.listen(8080)
+const config = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
+app.use((err,req,res,next)=>{
+    console.log(err)
+    const status = error.statusCode || 500
+    const message = error.message
+    res.status(status).json({message:message})
+})
+mongoose.connect("mongodb://127.0.0.1:27017/messages",config)
+.then((result)=>{
+    console.log("Listening on 8080")
+    app.listen(8080)
+}).catch(err=> console.log(err))
+
 
--- controllers/feed.js
+++ controllers/feed.js
@@ -1,6 +1,14 @@
 const {validationResult} = require("express-validator/check")
-
+const Post = require("../models/post")
 exports.getPosts = (req,res,next) => {
+    Post.find().then(posts=>{
+        res.status(200).json({message:"Fetched posts successfully",posts:posts})
+    }).catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500
+        }
+        next(err)
+    })
     res.status(200).json({
         posts:[{title:"first post",content:"Content",imageUrl:"images/image1.png",creator:{
             name:"karan"
@@ -12,13 +20,45 @@ exports.getPosts = (req,res,next) => {
 exports.createPost = (req,res,next) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
-        return res.status(422).json({message:"Validation failed,entered data is incorrect",
-        errors:errors.array()})
+        const error = new Error("Validation failed,entered data is incorrect")
+        error.statusCode = 422
+        throw error
     }
     const title = req.body.title
     const content = req.body.content
-    res.status(201).json({
-        message:"Post created Successfully",
-        post: {_id:new Date().toISOString(),title:title,content:content,creator:{name:"Karan"},createdAt:new Date()}
+    const post = new Post({
+        title:title,
+        content:content,
+        imageUrl:"images/image1.png",
+        creator:{name:"Karan"}
+    })
+    post.save().then(result=>{
+        res.status(201).json({
+            message:"Post created Successfully",
+            post:result})
+    }).catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500
+        }
+        next(err)
+    })
+    
+}
+
+exports.getPost = (req,res,next) =>{
+    const postId = req.params.postId
+    Post.findById(postId).then(post=>{
+        if(!post){
+            const error = new Error("Could not find post")
+            error.statusCode = 404
+            // throwing error will send it to catch block
+            throw error
+        }
+        res.status(200).json({message:"Post Fetched",post:post})
+    }).catch(err=>{
+        if(!err.statusCode){
+            err.statusCode = 500
+        }
+        next(err)
     })
 }
