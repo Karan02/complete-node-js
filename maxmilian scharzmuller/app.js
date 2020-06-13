1c7974621a7f4a84749647c7b6caa8ef8530a323
Karan02 <krnptl1234@gmail.com>
2020-06-13 20:39:34 +0530

    frontend and backend

--- maxmilian scharzmuller/app.js
+++ maxmilian scharzmuller/app.js
@@ -54,7 +54,11 @@ app.use((err,req,res,next)=>{
 mongoose.connect("mongodb://127.0.0.1:27017/messages",config)
 .then((result)=>{
     console.log("Listening on 8080")
-    app.listen(8080)
+    const server = app.listen(8080)
+    const io = require("socket.io")(server)
+    io.on("connection",socket =>{
+        console.log("Client connected")
+    })
 }).catch(err=> console.log(err))
 
 
