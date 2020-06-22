b543560208545e6a105008d209ac64be27ad2b2e
Karan02 <krnptl1234@gmail.com>
2020-06-22 19:10:10 +0530

    typescript-basic

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
-// we defined type module in package.json
-import fs from "fs"  
-import express from "express"
-import { resHandler } from "./response-handler.js"
-const app = express();
-
-app.get('/', resHandler);
-
-app.listen(3000);
+"use strict";
+const num1Element = document.getElementById("num1");
+const num2Element = document.getElementById("num2");
+// add ! to ignore null case
+const buttonElement = document.querySelector("button");
+// const numResults: number[] = []
+const numResults = [];
+const textResults = [];
+function add(num1, num2) {
+    if (typeof num1 === "number" && typeof num2 === "number") {
+        return num1 + num2;
+    }
+    else if (typeof num1 === "string" && typeof num2 === "string") {
+        return num1 + " " + num2;
+    }
+    return +num1 + +num2;
+}
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
+buttonElement.addEventListener("click", () => {
+    const num1 = num1Element.value;
+    const num2 = num2Element.value;
+    const result = add(+num1, +num2);
+    numResults.push(result);
+    const stringResult = add(num1, num2);
+    console.log(stringResult);
+    console.log(result);
+    textResults.push(stringResult);
+    printResult({ val: result, timestamp: new Date() });
+});
+const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("It worked");
+    }, 1000);
+});
+myPromise.then((result) => {
+    console.log(result);
+});
